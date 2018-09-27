1143facc9ec10813034ec5cf32b2d925cb7174b6
vbelle <vbelle@teksystems.com>
2018-09-27 17:40:36 +0530

    added django project

---
+++ mysite/hello/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class Flight(models.Model):
+    origin = models.CharField(max_length=64)
+    destination= models.CharField(max_length=64)
+    duration = models.IntegerField()
