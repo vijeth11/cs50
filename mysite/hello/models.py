a2ed1ab918980e195c70f902e9b83feaa2373ee0
vbelle <vbelle@teksystems.com>
2018-09-27 19:56:55 +0530

    added database and template

--- mysite/hello/models.py
+++ mysite/hello/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Airport(models.Model):
+    code=models.CharField(max_length=3)
+    city=models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.code+" ( "+self.city+" )"
 class Flight(models.Model):
-    origin = models.CharField(max_length=64)
-    destination= models.CharField(max_length=64)
+    origin = models.ForeignKey(Airport,on_delete=models.CASCADE,related_name="departures")
+    destination= models.ForeignKey(Airport,on_delete=models.CASCADE,related_name="arrival")
     duration = models.IntegerField()
+
+    def __str__(self):
+        return str(self.id) +" - "+str(self.origin)+" to "+str(self.destination)
--- mysite/hello/views.py
+++ mysite/hello/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Flight
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("hello world")
+    context={
+        "flights":Flight.objects.all()
+    }
+    #return HttpResponse("hello world")
+    return render(request,"flights/index.html",context)
