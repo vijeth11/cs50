42a45e30915b25b138ea9bbdda943d34a1114977
vijeth belle <vijeth11>
2019-03-21 09:06:53 -0400

    added changes

--- project1/server/app.py
+++ project1/server/app.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/david')
+def david():
+    return "hell david"
+
+
+@app.route('/books')
+@app.route('/books/<string:name>')
+def book_index(name=''):
+    print(name);
+    if name == '':
+        return render_template("index.html")
+    return render_template(f"{name}")
+
