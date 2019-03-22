4a526462fa243aa78fae1a0dcc9e770ae78f1d07
jagatguru31 <jagatguru31@gmail.com>
2019-03-23 00:52:08 +0530

    updates

--- project1/server/app.py
+++ project1/server/app.py
@@ -11,7 +11,7 @@ except ImportError:
     import csv
     import psycopg2
 
-DATABASE_URL = "postgres://exjjkzmxbgaznj:a055be0c06169b7760b425694134f75318ff7750a3089b6ca686421e30ce7ca9@ec2-79-125-4-72.eu-west-1.compute.amazonaws.com:5432/d746m01p7ppuvo"
+DATABASE_URL = "postgres://kvodqjxngtsgpt:3f8bc0ed76c932265edff00946122e5386ad95fc7553ba5a7cdeb19c453a65da@ec2-54-246-92-116.eu-west-1.compute.amazonaws.com:5432/d2e8i4j1a7bhqu"
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 app = Flask(__name__)
 
@@ -42,8 +42,8 @@ def book_index(name=''):
     if name == '':
         return render_template("index.html")
     if "Search" in name:
-        data = cur.execute("select * from books")
-        print(data)
+        #data = cur.execute("select * from books")
+        #print(data)
         return render_template(f"{name}",tests=test)
     if "details" in name:
         value=request.args.get('value')
--- project1/server/import.py
+++ project1/server/import.py
@@ -7,14 +7,17 @@ except ImportError:
     os.system('pip3 install psycopg2-binary')
     import psycopg2
 
-DATABASE_URL = "postgres://exjjkzmxbgaznj:a055be0c06169b7760b425694134f75318ff7750a3089b6ca686421e30ce7ca9@ec2-79-125-4-72.eu-west-1.compute.amazonaws.com:5432/d746m01p7ppuvo"
+DATABASE_URL = "postgres://kvodqjxngtsgpt:3f8bc0ed76c932265edff00946122e5386ad95fc7553ba5a7cdeb19c453a65da@ec2-54-246-92-116.eu-west-1.compute.amazonaws.com:5432/d2e8i4j1a7bhqu"
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 cur = conn.cursor()
+count=0
 with open('books.csv') as csvfile:
         readCSV = csv.reader(csvfile)
         for rows in readCSV:
             if rows[0] != "isbn":
-                res = cur.execute("insert into books values('" + str(rows[0]) + "','" + str(rows[1]).replace("'","\"") + "','" + str(rows[2]).replace("'","\"") + "'," + rows[3] + ");")
+                res = cur.execute("insert into books (isbn,author,title,year)values('" + str(rows[0]) + "','" + str(rows[1]).replace("'","\"") + "','" + str(rows[2]).replace("'","\"") + "'," + rows[3] + ");")
                 print(res)
-
+                count+=1
+                print(count)
+                conn.commit()
 print("inserted")
