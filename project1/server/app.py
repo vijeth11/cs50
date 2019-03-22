0f0643828b295daefce298be1ac3fdef062a80e7
vijeth belle <vijeth11>
2019-03-22 07:45:10 -0400

    added books

--- project1/server/app.py
+++ project1/server/app.py
@@ -1,11 +1,18 @@
 import os
 try:
    from flask import Flask,render_template,redirect,request
+   import csv
 except ImportError:
     os.system('pip3 install flask')
     os.system('pip3 install django')
 app = Flask(__name__)
 
+test=[]
+with open('books.csv') as csvfile:
+    readCSV = csv.reader(csvfile)
+    for rows in readCSV:
+        if rows[0]!="isbn":
+           test.append(rows)
 
 @app.route('/')
 def hello_world():
@@ -20,13 +27,18 @@ def david():
 @app.route('/books/<string:name>')
 def book_index(name=''):
     print(name);
-    test=[["Otto","hello","world","1234"],["second","value","test","1235"]]
+
+
     if name == '':
         return render_template("index.html")
     if "Search" in name:
         return render_template(f"{name}",tests=test)
     if "details" in name:
         value=request.args.get('value')
-        return render_template(f"{name}", value=value)
+        for t in test:
+            if t[0]==value:
+                book=t
+                break
+        return render_template(f"{name}", value=value,test=book)
     return render_template(f"{name}")
 
