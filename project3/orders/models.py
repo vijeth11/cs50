d0a68c4c9e41a047638fca9c683bdec9aa49ec3d
vijeth11 <vijeth11@yahoo.co.in>
2020-06-25 14:50:25 -0400

    added drop down options for the pizzatype

--- project3/orders/models.py
+++ project3/orders/models.py
@@ -2,13 +2,16 @@ from django.db import models
 
 # Create your models here.
 
+PizzaTypes=[("R","Regular"),
+            ("S","Sicilian")
+            ]
 class Toppings(models.Model):
     name = models.CharField(max_length=100)
 class Pizza(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits= 10,decimal_places=2)
     image = models.ImageField(upload_to="pizza")
-    typeOfPizza = models.BinaryField()
+    typeOfPizza = models.CharField(max_length=1,choices= PizzaTypes)
     selectedTopings = models.ManyToManyField(Toppings)
 
     def __str__(self):
