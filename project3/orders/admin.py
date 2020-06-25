f3060632ce491f705c59c00f923bf54602c970cb
vijeth11 <vijeth11@yahoo.co.in>
2020-06-25 14:19:39 -0400

    added models for pizza

--- project3/orders/admin.py
+++ project3/orders/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Pizza, Toppings
 # Register your models here.
+
+class PizzaAdmin(admin.ModelAdmin):
+      filter_horizontal = ("selectedTopings",)
+
+admin.site.register(Pizza,PizzaAdmin)
+admin.site.register(Toppings)
--- project3/orders/models.py
+++ project3/orders/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Toppings(models.Model):
+    name = models.CharField(max_length=100)
+class Pizza(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits= 10,decimal_places=2)
+    image = models.ImageField(upload_to="pizza")
+    typeOfPizza = models.BinaryField()
+    selectedTopings = models.ManyToManyField(Toppings)
+
+    def __str__(self):
+        return self.name
+
+
+
