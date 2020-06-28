8a6d9474b6976d049e16085824e82db1bea16d1f
vijeth11 <vijeth11@yahoo.co.in>
2020-06-28 06:34:59 -0400

    added account part

--- project3/orders/static/js/menu.js
+++ project3/orders/static/js/menu.js
@@ -5,3 +5,20 @@ window.addEventListener('scroll', function() {
         el.classList.add("active");
     }
   });
+
+  function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
