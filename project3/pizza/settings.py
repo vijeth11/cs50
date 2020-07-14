44b38fbf9dfbe283c1b714be9249cbb901d5489d
vijeth <vijeth11@yahoo.co.in>
2020-07-14 12:47:00 +1200

    Initial commit

--- project3/pizza/settings.py
+++ project3/pizza/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -140,3 +141,4 @@ MEDIA_URL = '/media/'
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+django_heroku.settings(locals())
