33e397c6380416972239251b40a166606093cbe5
vijeth11 <vijeth11@yahoo.co.in>
2020-08-23 17:30:22 +0530

    fix

--- project4/vegfoods/vegfoods/static/js/base.js
+++ project4/vegfoods/vegfoods/static/js/base.js
@@ -2,7 +2,7 @@ window.onscroll = function(){ scrollFunction();}
 
 function scrollFunction()
 {
-    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
         document.getElementById("co-navbar").classList.add("scrolled");
       } else {
         document.getElementById("co-navbar").classList.remove("scrolled");
